8079090d5c2a4f2c30f15b0130d94244e845dc74
Denis <youtipie@gmail.com>
2024-04-30 10:35:59 +0300

    Add pagination

--- app/routes.py
+++ app/routes.py
@@ -15,16 +15,31 @@ def load_user(user_id):
 @app.route("/")
 def index():
     categories = db.session.query(Category).all()
-    return render_template("index.html", games=db.session.query(Game).all(), heading="Найпопулярніші ігри",
-                           categories=categories, generate_public_url=generate_public_url)
+    page = request.args.get('page', 1, type=int)
+    games = db.session.query(Game).paginate(page=page, per_page=5, error_out=False)
+    next_url = url_for('index', page=games.next_num) \
+        if games.has_next else None
+    prev_url = url_for('index', page=games.prev_num) \
+        if games.has_prev else None
+    return render_template("index.html", games=games, heading="Найпопулярніші ігри",
+                           categories=categories, generate_public_url=generate_public_url,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route("/category/<string:category_name>")
 def category(category_name: str):
     categories = db.session.query(Category).all()
-    return render_template("index.html", games=db.session.query(Category).filter_by(name=category_name).first().games,
+    page = request.args.get('page', 1, type=int)
+    games = db.session.query(Game).join(Category).filter_by(name=category_name).paginate(page=page, per_page=5,
+                                                                                         error_out=False)
+    next_url = url_for('category', category_name=category_name, page=games.next_num) \
+        if games.has_next else None
+    prev_url = url_for('category', category_name=category_name, page=games.prev_num) \
+        if games.has_prev else None
+    return render_template("index.html", games=games,
                            heading=category_name.title(), categories=categories,
-                           generate_public_url=generate_public_url)
+                           generate_public_url=generate_public_url,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route("/login", methods=["GET", "POST"])
