c913d62d6a1efc646d8f92ad79507aafabb02991
Denis <youtipie@gmail.com>
2024-05-21 09:04:49 +0300

    Add tests for API

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from app.models import User, Category, Game, Image, Comment
 from app.aws import generate_public_url
 from config import Config
 from sqlalchemy import text
+import json
 
 
 class TestConfig(Config):
@@ -16,6 +17,7 @@ class BaseTestCase(unittest.TestCase):
         self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
         self.app_context.push()
+        db.drop_all()
         db.create_all()
         db.session.execute(text('CREATE EXTENSION IF NOT EXISTS pg_trgm'))
 
@@ -216,6 +218,137 @@ class CategoryTestCase(BaseTestCase):
 
         self.assertEqual(len(self.category.games), 1)
         self.assertEqual(self.category.games[0], game)
+
+
+class APITestCase(BaseTestCase):
+    def setUp(self) -> None:
+        super(APITestCase, self).setUp()
+        self.client = self.app.test_client()
+        self.category = Category(name="Action")
+        db.session.add(self.category)
+        db.session.commit()
+
+        self.category2 = Category(name="RPG")
+        db.session.add(self.category2)
+        db.session.commit()
+
+        for i in range(self.app.config['ITEMS_PER_PAGE'] + self.app.config['ITEMS_PER_PAGE'] // 2):
+            self.game = Game(
+                title="Sample Game",
+                category_id=self.category.id if i != 0 else self.category2.id,
+                description="A fun game",
+                is_paid=True,
+                version="1.0",
+                apk_name=f"sample_game.apk{i}",
+                apk_size=50.5,
+                folder_name=f"sample_game{i}"
+            )
+            db.session.add(self.game)
+        db.session.commit()
+
+        self.user = User(username="john", email="john@example.com")
+        self.user.set_password("password")
+        db.session.add(self.user)
+        db.session.commit()
+
+        for i in range(self.app.config['ITEMS_PER_PAGE'] + self.app.config['ITEMS_PER_PAGE'] // 2):
+            self.comment = Comment(content="Great game!", user_id=self.user.id, game_id=self.game.id)
+            db.session.add(self.comment)
+        db.session.commit()
+
+    def test_get_games(self):
+        # Test successful retrieval of games
+        response = self.client.get('/api/games')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertTrue(data['success'])
+        self.assertEqual(len(data['games']), self.app.config['ITEMS_PER_PAGE'])
+        self.assertEqual(data['total_games'],
+                         self.app.config['ITEMS_PER_PAGE'] + self.app.config['ITEMS_PER_PAGE'] // 2)
+        self.assertEqual(data['next_url'], 2)
+        self.assertIsNone(data['prev_url'])
+
+        # Test pagination
+        response = self.client.get('/api/games?page=2')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertTrue(data['success'])
+        self.assertEqual(len(data['games']), self.app.config['ITEMS_PER_PAGE'] - self.app.config['ITEMS_PER_PAGE'] // 2)
+        self.assertEqual(data['prev_url'], 1)
+        self.assertIsNone(data['next_url'])
+
+        # Test pagination redirect
+        response = self.client.get('/api/games?page=99')
+        self.assertEqual(response.status_code, 302)
+
+        # Test invalid order parameter
+        response = self.client.get('/api/games?order=invalid')
+        self.assertEqual(response.status_code, 400)
+        data = json.loads(response.data)
+        self.assertFalse(data['success'])
+
+        # Test filtering by category
+        response = self.client.get('/api/games?category=RPG')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertTrue(data['success'])
+        self.assertEqual(len(data['games']), 1)
+        self.assertEqual(len(data['games']), data['total_games'])
+
+        # Test invalid category
+        response = self.client.get('/api/games?category=Invalid')
+        self.assertEqual(response.status_code, 400)
+        data = json.loads(response.data)
+        self.assertFalse(data['success'])
+
+    def test_add_popularity(self):
+        # Test successful addition of popularity
+        token = self.game.create_popularity_token()
+        response = self.client.get(f'/api/add_popularity?token={token}')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertTrue(data['success'])
+        self.assertEqual(self.game.popularity, 1)
+
+        # Test invalid token
+        response = self.client.get('/api/add_popularity?token=invalid')
+        self.assertEqual(response.status_code, 400)
+        data = json.loads(response.data)
+        self.assertFalse(data['success'])
+
+    def test_get_comments(self):
+        # Test successful retrieval of comments
+        response = self.client.get(f'/api/comments?game_id={self.game.id}')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertTrue(data['success'])
+        self.assertEqual(len(data['comments']), self.app.config['ITEMS_PER_PAGE'])
+        self.assertEqual(data['total_games'],
+                         self.app.config['ITEMS_PER_PAGE'] + self.app.config['ITEMS_PER_PAGE'] // 2)
+        self.assertEqual(data['next_url'], 2)
+        self.assertIsNone(data['prev_url'])
+
+        # Test invalid game_id
+        response = self.client.get('/api/comments?game_id=999')
+        self.assertEqual(response.status_code, 404)
+        data = json.loads(response.data)
+        self.assertFalse(data['success'])
+
+        # Test pagination
+        response = self.client.get(f'/api/comments?game_id={self.game.id}&page=2')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertTrue(data['success'])
+        self.assertEqual(len(data['comments']), self.app.config['ITEMS_PER_PAGE'] - self.app.config['ITEMS_PER_PAGE'] // 2)
+        self.assertEqual(data['prev_url'], 1)
+        self.assertIsNone(data['next_url'])
+
+        # Test pagination redirect
+        response = self.client.get(f'/api/comments?game_id={self.game.id}&page=99')
+        self.assertEqual(response.status_code, 302)
+        # data = json.loads(response.data)
+        # self.assertTrue(data['success'])
+        # self.assertEqual(len(data['comments']), 1)
 
 
 if __name__ == '__main__':
