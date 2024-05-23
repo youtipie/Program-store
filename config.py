189d9d461bf70113ad99fa642dbb1b7f7000c3b2
Denis <youtipie@gmail.com>
2024-05-23 10:23:35 +0300

    Fix uuid creation

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or uuid.uuid4()
+    SECRET_KEY = os.environ.get("SECRET_KEY") or str(uuid.uuid4())
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     TEST_DATABASE_URI = os.environ.get("TEST_DATABASE_URI") or "sqlite://"
     SQLALCHEMY_TRACK_MODIFICATIONS = int(os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS") or False)
