44dceed3625223943051fe94fd14314f74554078
Denis <youtipie@gmail.com>
2024-05-28 19:38:24 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Deploy command failed, retrying in 5 secs...
+    sleep 5
+done
 exec gunicorn -b :5000 --access-logfile - --error-logfile - main:app
